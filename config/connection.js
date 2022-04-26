4ca601d32122edba58470403780c6d487c02dcfd
Ben Fok <benfok8@gmail.com>
2022-04-26 13:51:58 -0700

    comments

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-    // 'mongodb://localhost:27017/socialNetworkDB'
     process.env.MONGODB_STRING,
     {
         useNewUrlParser: true, 
